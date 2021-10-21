c659fa71833fd4e8716518981d7b73e9f3dbfdb8
“TorrLaura” <laplebe35@gmail.com>
2021-10-20 22:04:02 -0500

    Completed spreadDestructFuncExercise

---
+++ script.js
@@ -0,0 +1,15 @@
+const jPDinos = [
+    "Velociraptors", 
+    "Tyrannosaurus Rex", 
+    "Dilophosaurus"
+];
+console.log(jPDinos);
+
+function seeDinos = {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+seeDinos(jPDinos);
+
+
